aca364ab15fef1b1703aa4b6515a703c468bc076
Yehya Usman <yehya.dev@gmail.com>
2021-08-15 21:36:33 +0530

    fix origin

--- cosmic_crypto_webservice/webservice.py
+++ cosmic_crypto_webservice/webservice.py
@@ -21,7 +21,7 @@ redis_cli = Redis()
 app.add_middleware(
     CORSMiddleware,
     allow_credentials=True,
-    allow_origins=["*"],
+    allow_origins=["http://localhost:3000"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
